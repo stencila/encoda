9061686157ca96ffb3c7add6982e772d8b039852
Nokome Bentley <nokome@stenci.la>
2017-12-09 15:25:32 +1300

    Add usage

--- bin/stencila-convert.js
+++ bin/stencila-convert.js
@@ -1,3 +1,15 @@
+#!/usr/bin/env node
+
 const convert = require('../src').convert
 
-convert(process.argv[2], process.argv[3])
+const pathFrom = process.argv[2]
+const pathTo = process.argv[3]
+
+if (!pathTo || !pathFrom) {
+  console.error('Usage:\n\n  stencila-convert <from> <to>\n\ne.g.\n')
+  console.error('  stencila-convert doc.md doc.jats.xml')
+  console.error('  stencila-convert data.sheet.xml data.csv')
+  process.exit(1)
+}
+
+convert(pathFrom, pathTo)
