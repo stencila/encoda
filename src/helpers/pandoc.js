bc45d8de46ab524ac5079ddbe7895ca6cf8d7889
Nokome Bentley <nokome@stenci.la>
2018-01-17 18:52:21 +1300

    Pandoc: check for global; install in `~/.stencila` folder
    Use already installed Pandoc if version is OK.

--- src/helpers/pandoc.js
+++ src/helpers/pandoc.js
@@ -1,26 +1,50 @@
 const BinWrapper = require('bin-wrapper')
+const binVersionCheck = require('bin-version-check')
 const fs = require('fs')
 const path = require('path')
 const childProcess = require('child_process')
 
-const pandoc = new BinWrapper({ global: false })
+const pandoc = new BinWrapper()
+  // Semver requirement for Pandoc
+  .version('>=2.0.4')
+  // Sources for downloaded binaries (if necessary)
   .src('https://github.com/stencila/pandoc/releases/download/jats-8/pandoc-linux.tar.gz', 'linux', 'x64')
   .src('https://github.com/stencila/pandoc/releases/download/jats-8/pandoc-macOS.zip', 'darwin')
   .src('https://github.com/stencila/pandoc/releases/download/jats-8/pandoc-windows-i386.zip', 'win32')
-  .dest(path.join(__dirname, '..', '..', 'vendor'))
+  .dest((function () {
+    // Destination for downloaded binaries (if necessary)
+    switch (process.platform) {
+      case 'darwin':
+        return path.join(process.env.HOME, 'Library', 'Application Support', 'Stencila')
+      case 'linux':
+        return path.join(process.env.HOME, '.stencila')
+      case 'win32':
+        return path.join(process.env.APPDATA, 'Stencila')
+      default:
+        return path.join(process.env.HOME, 'stencila')
+    }
+  }()))
   .use(process.platform === 'win32' ? 'pandoc.exe' : 'bin/pandoc')
 
-fs.access(pandoc.path(), function (err) {
-  if (err) {
-    console.log('⧗ Downloading Pandoc (this could take some time).')
-    pandoc.run(['--version'], function (err) {
-      if (err) {
-        console.log('✗ Download fail :(')
-      } else {
-        console.log('✓ Download success!')
-      }
-    })
-  }
+// Check that an acceptable version of Pandoc is available on PATH
+binVersionCheck('pandoc', pandoc.version()).then(() => {
+  // Use global Pandoc - set `pandoc.path()` to 'pandoc'
+  pandoc.dest('')
+  pandoc.use('pandoc')
+}).catch(() => {
+  // Global pandoc is not suitable, check for local Pandoc
+  fs.access(pandoc.path(), function (err) {
+    if (err) {
+      console.log('⧗ Downloading Pandoc (this could take some time).')
+      pandoc.run(['--version'], function (err) {
+        if (err) {
+          console.log('✗ Download fail :(')
+        } else {
+          console.log('✓ Download success!')
+        }
+      })
+    }
+  })
 })
 
 pandoc.spawn = function (input, args) {
