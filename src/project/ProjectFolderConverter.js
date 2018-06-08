0bc388fecea930d88b6bb143feebe2e5d4c762a4
Nokome Bentley <nokome@stenci.la>
2018-06-08 18:50:05 +1200

    async/await-ify project converter

--- src/project/ProjectFolderConverter.js
+++ src/project/ProjectFolderConverter.js
@@ -18,50 +18,45 @@ class ProjectFolderConverter extends ProjectConverter {
     })
   }
 
-  import (pathFrom, pathTo, volumeFrom = fs, volumeTo = null, options = {}) {
+  async import (pathFrom, pathTo, volumeFrom = fs, volumeTo = null, options = {}) {
     pathTo = pathTo || pathFrom
     volumeTo = volumeTo || volumeFrom
     options.include = options.include || '*'
 
-    return new Promise((resolve, reject) => {
-      // Get the list of files in the directory using the `include` and `exclude`
-      // glob options
-      glob(path.join(pathFrom, options.include), {
-        ignore: options.exclude
-      }, (err, files) => {
-        if (err) return reject(err)
+    // Get the list of files in the directory using the `include` and `exclude`
+    // glob options
+    const files = glob.sync(path.join(pathFrom, options.include), {
+      ignore: options.exclude
+    })
 
-        // If a file can be imported then import it recording source and destination file paths
-        const converters = this.converters
-        const promises = []
-        function conversionPromise (filePathFrom, index = 0) {
-          const Converter = converters[index]
-          if (!Converter) return Promise.resolve({pathTo: null})
-          let converter = new Converter()
-          return converter.canImport(filePathFrom, volumeFrom).then((can) => {
-            if (!can) return conversionPromise(filePathFrom, index + 1)
-            else return converter.import(filePathFrom, null, volumeFrom, volumeTo)
-          })
-        }
-        for (let filePathFrom of files) promises.push(conversionPromise(filePathFrom))
+    // If a file can be imported then import it recording source and destination file paths
+    const converters = this.converters
+    const promises = []
+    async function conversionPromise (filePathFrom, index = 0) {
+      const Converter = converters[index]
+      if (!Converter) return {pathTo: null}
 
-        // Create project manifest
-        Promise.all(promises).then(conversions => {
-          return this.createDom().then(manifest => {
-            let documents = manifest('documents')
-            for (let conversion of conversions) {
-              if (!conversion.pathTo) continue
-              const filePathTo = path.relative(pathFrom, conversion.pathTo)
-              const filePathFrom = path.relative(pathFrom, conversion.pathFrom)
-              const type = conversion.formatTo
-              const id = filePathFrom
-              const name = filePathFrom
-              documents.append(`<document id="${id}" name="${name}" type="${type}" path="${filePathTo}"/>`)
-            }
-            return this.writeXml(path.join(pathTo, 'manifest.xml'), manifest)
-          })
-        })
-      })
+      let converter = new Converter()
+      const can = await converter.canImport(filePathFrom, volumeFrom)
+      if (!can) return conversionPromise(filePathFrom, index + 1)
+      else return converter.import(filePathFrom, null, volumeFrom, volumeTo)
+    }
+    for (let filePathFrom of files) promises.push(conversionPromise(filePathFrom))
+
+    // Create project manifest
+    Promise.all(promises).then(conversions => {
+      const manifest = this.createDom()
+      let documents = manifest('documents')
+      for (let conversion of conversions) {
+        if (!conversion.pathTo) continue
+        const filePathTo = path.relative(pathFrom, conversion.pathTo)
+        const filePathFrom = path.relative(pathFrom, conversion.pathFrom)
+        const type = conversion.formatTo
+        const id = filePathFrom
+        const name = filePathFrom
+        documents.append(`<document id="${id}" name="${name}" type="${type}" path="${filePathTo}"/>`)
+      }
+      return this.writeXml(path.join(pathTo, 'manifest.xml'), manifest)
     })
   }
 }
