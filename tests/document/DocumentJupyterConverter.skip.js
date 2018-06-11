c5b86a8c351c0ea250c978f166af664957a90d62
Nokome Bentley <nokome@stenci.la>
2018-06-11 18:42:50 +1200

    Remove old tests

--- tests/document/DocumentJupyterConverter.skip.js
+++
@@ -1,7 +0,0 @@
-const DocumentJupyterConverter = require('../../src/document/DocumentJupyterConverter')
-const helpers = require('../helpers')
-
-const converter = new DocumentJupyterConverter()
-const { testImport } = helpers(converter, 'document')
-
-testImport('py-notebook/ipynb/py-notebook.ipynb', 'py-notebook/dar/py-notebook.jats.xml')
--- tests/document/DocumentRmdConverter.skip.js
+++
@@ -1,7 +0,0 @@
-const DocumentRmdConverter = require('../../src/document/DocumentRmdConverter')
-const helpers = require('../helpers')
-
-const converter = new DocumentRmdConverter()
-const { testImport } = helpers(converter, 'document')
-
-testImport('r-notebook/rmd/r-notebook.rmd', 'r-notebook/dar/r-notebook.jats.xml')
