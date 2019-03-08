0376bd1b9a0c8cdb63554004697f5dcca5b776a8
Nokome Bentley <nokome@stenci.la>
2019-03-08 15:15:27 +1300

    refactor: initial move to unified and Typescript

---
+++ src/index.ts
@@ -0,0 +1,112 @@
+// @ts-ignore
+import mime from 'mime-types'
+import { VFile } from 'vfile'
+
+import { Node } from './sast'
+
+/**
+ * The interface for a compiler.
+ * 
+ * A compiler is simply a module with these constants
+ * and functions (some of which are optional).
+ * 
+ * Note that our use of the term "compiler", is consistent with our usage elsewhere in Stencila
+ * as something that creates or modifies executable document, and
+ * differs from that used by [`unified`](https://github.com/unifiedjs/unified#processorcompiler).
+ */
+interface Compiler {
+  /**
+   * An array of [IANA Media Type](https://www.iana.org/assignments/media-types/media-types.xhtml)
+   * that the compiler can parse/unparse
+   */
+  media: Array<string>
+
+  /**
+   * A function that does [content sniffing](https://en.wikipedia.org/wiki/Content_sniffing)
+   * of the `VFile` to determine if the compiler is able to parse the content.
+   */
+  sniff?: (file: VFile) => Promise<boolean>
+
+  /**
+   * Parse the content of a `Vfile` into a tree of Stencila
+   * document nodes
+   * 
+   * @param file The `VFile` to parse
+   * @returns The root of the document
+   */
+  parse?: (file: VFile) => Promise<Node>
+
+  /**
+   * Unparse a Stencila document node to a `VFile`.
+   * 
+   * @param node The document node to unparse
+   * @returns The `VFile` with generated `contents`
+   */
+  unparse?: (node: Node) => Promise<VFile>
+}
+
+
+/**
+ * A list of all compilers.
+ * 
+ * Note that order is of importance when the media type of 
+ * a `VFile` can not be determined since the first compiler
+ * which returns `true` when `sniff`ing the file will be
+ * used to parse it.
+ */
+const compilers: Array<Compiler> = []
+
+/**
+ * A map of media types to compiler.
+ * 
+ * Used for first attempt at revolving the compiler
+ * to use to parse/unparse from/to a `VFile`.
+ */
+const lookup: {[key: string]: Compiler} = {}
+for (let compiler of compilers) {
+  for (let medium of compiler.media) {
+    lookup[medium] = compiler
+  }
+}
+
+/**
+ * Resolve the compiler to use to parse/unparse from/to a `VFile`.
+ * 
+ * Resolution is attempted on the basis of `media` types first and then, if that
+ * fails, using the `sniff()` function.
+ * 
+ * @param file The `VFile` to resolve for
+ * @returns The `Compiler` to use
+ */
+export function resolve (file: VFile): Compiler {
+  let mediaType = (file as any).mediaType
+  
+  if (!mediaType && file.extname) {
+    mediaType = mime.lookup(file.extname)
+  }
+
+  if (mediaType) {
+    let compiler = lookup[mediaType]
+    if (compiler) return compiler
+  }
+
+  for (let compiler of compilers) {
+    if (compiler.sniff && compiler.sniff(file)) {
+      return compiler
+    }
+  }
+
+  throw new Error(`No compiler could be found for file ${file.path}`)
+}
+
+export async function parse (file: VFile): Promise<Node> {
+  const compiler = resolve(file)
+  if (!compiler.parse) throw new Error(`Not able to parse`)
+  return await compiler.parse(file)
+}
+
+export async function unparse (node: Node, file: VFile): Promise<VFile> {
+  const compiler = resolve(file)
+  if (!compiler.unparse) throw new Error(`Not able to unparse`)
+  return await compiler.unparse(node)
+}
---
+++ src/vfile.ts
@@ -0,0 +1,39 @@
+import vfile, { VFile } from 'vfile'
+// @ts-ignore
+import toVFile from 'to-vfile'
+
+/**
+ * Load a string into a virtual file
+ * 
+ * @param contents Contents to load
+ */
+export function load (contents: string): VFile {
+  return vfile({contents})
+}
+
+/**
+ * Dump a string from the contents of a virtual files
+ * 
+ * @param file The virtual file to dump
+ */
+export function dump (vfile: VFile): string {
+  return vfile.contents ? vfile.contents.toString() : ''
+}
+
+/**
+ * Read a file path into a virtual file
+ * 
+ * @param path File system path to read
+ */
+export async function read (path: string): Promise<VFile> {
+  return toVFile.read(path)
+}
+
+/**
+ * Write a virtual file to the file system
+ * 
+ * @param vfile Virtual file to write
+ */
+export async function write (vfile: VFile): Promise<any> {
+  return toVFile.write(vfile)
+}
