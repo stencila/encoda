845973e2938fa363e724c61d9e56645090f348d0
Nokome Bentley <nokome@stenci.la>
2019-03-12 16:20:29 +1300

    refactor: type specific transformation

--- src/sast-mdast.ts
+++ src/sast-mdast.ts
@@ -6,50 +6,65 @@ import * as Sast from './sast'
  * Transform a [Markdown Abstract Syntax Tree](https://github.com/syntax-tree/mdast) to
  * a Stencila AST
  *
- * @param mdast The MDAST tree to transform
+ * @param node The MDAST tree to transform
  */
-export function mdast2sast (mdast: Unist.Node): Sast.Node {
-  function walk (node: Unist.Node) {
-    if (!node.type) return
-    
-    // Rename types
-    switch (node.type) {
-      default:
-        node.type = node.type.charAt(0).toUpperCase() + node.type.slice(1)
-    }
-    // Rename `position` to `$position` so it is not serialised
-    if (node.position) {
-      node.$position = node.position
-      delete node.position
-    }
-
-    if (node.children) {
-      for (let child of node.children as Array<Unist.Node>) walk(child)
-    }
+export function mdast2sast (node: Unist.Node): Sast.Node {
+  switch (node.type) {
+    case 'root': return documentFromRoot(node)
+    default: return defaultFrom(node)
   }
-  walk(mdast)
-  return mdast
 }
 
 /**
  *  Transform a Stencila AST to a Markdown AST
  *
- * @param sast The SAST tree to transform
+ * @param node The SAST tree to transform
  */
-export function sast2mdast (sast: Sast.Node): Unist.Node {
-  function walk (node: Sast.Node) {
-    if (!node.type) return
-    
-    // Rename types
-    switch (node.type) {
-      default:
-        node.type = node.type.toLowerCase()
-    }
+export function sast2mdast (node: Sast.Node): Unist.Node {
+  switch (node.type) {
+    case 'Document': return documentToRoot(node)
+    default: return defaultTo(node)
+  }
+}
 
-    if (node.children) {
-      for (let child of node.children as Array<Sast.Node>) walk(child)
+function documentFromRoot (node: Unist.Node): Sast.Node {
+  return {
+    type: 'Document',
+    body: (node.children as Array<Unist.Node>).map(mdast2sast)
+  }
+}
+
+function documentToRoot (node: Sast.Node): Unist.Node {
+  return {
+    type: 'root',
+    children: (node.body as Array<Sast.Node>).map(sast2mdast)
+  }
+}
+
+function defaultFrom (mdast: Unist.Node): Sast.Node {
+  const sast: Sast.Node = {
+    type: mdast.type.charAt(0).toUpperCase() + mdast.type.slice(1)
+  }
+  for (let [key, child] of Object.entries(mdast)) {
+    if (!['type', 'position'].includes(key)) {
+      if (Array.isArray(child)) sast[key] = (child as Array<Unist.Node>).map(mdast2sast)
+      else if (typeof child === 'object') sast[key] = mdast2sast(child as Unist.Node)
+      else sast[key] = child
     }
   }
-  walk(sast)
-  return sast as Unist.Node
+  return sast
+}
+
+function defaultTo (sast: Sast.Node): Unist.Node {
+  const mdast: Unist.Node = {
+    type: sast.type.toLowerCase()
+  }
+  for (let [key, child] of Object.entries(sast)) {
+    if (!['type'].includes(key)) {
+      if (Array.isArray(child)) mdast[key] = (child as Array<Unist.Node>).map(sast2mdast)
+      else if (typeof child === 'object') mdast[key] = sast2mdast(child as Unist.Node)
+      else mdast[key] = child
+    }
+  }
+  return mdast
 }
