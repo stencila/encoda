a4b393c8068ec16260e2422522417dccfaaa709c
Alex Ketch <alex@ketch.me>
2019-04-29 15:22:12 -0400

    Remove duplicate test case

--- tests/html.test.ts
+++ tests/html.test.ts
@@ -28,39 +28,10 @@ const eg1 = {
   }
 }
 
-const eg2 = {
-  string: `<html>
-
-<head></head>
-
-<body>
-    <p>Paragraph one.</p>
-</body>
-
-</html>`,
-
-  node: {
-    type: 'Article',
-    content: [
-      {
-        type: 'Paragraph',
-        content: [
-          {
-            type: '#text',
-            value: 'Paragraph one.'
-          }
-        ]
-      }
-    ]
-  }
-}
-
 test('parse', async () => {
   expect(await parse(load(eg1.string))).toEqual(eg1.node)
-  expect(await parse(load(eg2.string))).toEqual(eg2.node)
 })
 
 test('unparse', async () => {
   expect(dump(await unparse(eg1.node))).toEqual(eg1.string)
-  expect(dump(await unparse(eg2.node))).toEqual(eg2.string)
 })
