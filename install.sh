28db94e7468aaf61d69227090baf95cd27eb74cb
Nokome Bentley <nokome@stenci.la>
2019-05-21 14:28:43 +1200

    chore: fix install message

--- install.sh
+++ install.sh
@@ -36,7 +36,7 @@ if [[ "$OS" == "Linux" || "$OS" == "Darwin" ]]; then
     # Unpack `node_modules` etc into the $INSTALL_PATH/convert-$VERSION
     $INSTALL_PATH/stencila-convert-$VERSION/stencila-convert --version
     
-    echo "Pointing stencila-convert to $INSTALL_PATH/convert-$VERSION/convert"
+    echo "Pointing stencila-convert to $INSTALL_PATH/stencila-convert-$VERSION/stencila-convert"
     ln -sf stencila-convert-$VERSION/stencila-convert $INSTALL_PATH/stencila-convert
 else
     echo "Sorry, I don't know how to install on this OS, please see https://github.com/stencila/convert#install"
