baa459b2acecf4fa4ef77ce1a7db421f99637902
Nokome Bentley <nokome@stenci.la>
2019-05-27 13:11:17 +1200

    tests: update test for issue 74

--- tests/issues/74-write-epipe.test.ts
+++ tests/issues/74-write-epipe.test.ts
@@ -1,11 +1,11 @@
-//import * as stencila from '@stencila/schema'
-//import fs from 'fs-extra'
+import fs from 'fs-extra'
 import path from 'path'
-import { read } from '../../src'
+import { read, write } from '../../src'
 
-describe('issue 74', () => {
+test('issue 74', async () => {
   /**
-   * The issue was reported with respect to converting to HTML
+   * The [issue](https://github.com/stencila/convert/issues/74)
+   * was reported with respect to converting to HTML
    * but it also occurs when converting to JSON.
    *
    * $ npx ts-node --files src/cli ./tests/issues/74-write-epipe.docx --to json
@@ -19,10 +19,21 @@ describe('issue 74', () => {
    * within `pandoc.run`.
    */
 
-  const file = path.join(__dirname, '74-write-epipe.docx')
+  const docx = path.join(__dirname, '74-write-epipe.docx')
+  const html = path.join(__dirname, '74-write-epipe.html')
 
-  test('can read file', async () => {
-    // Before bug fix, this fails with `Error: write EPIPE`
-    const node = await read(file)
-  })
+  // Before fix, this fails with `Error: write EPIPE`
+  //    https://travis-ci.org/stencila/convert/jobs/537572367#L172
+  //    https://ci.appveyor.com/project/nokome/convert/builds/24825307#L66
+  const node = await read(docx)
+  expect(node).toBeTruthy()
+
+  // The bug wasn't related to generating HTML, but just
+  // to cover everything in the issue...
+  await write(node, html)
+  expect(await fs.pathExists(html)).toBeTruthy()
+
+  // Cleanup
+  await fs.remove(html)
+  await fs.remove(docx + '.media')
 })
