4ae019d0a8b4d16744113629648c2e906961b827
Nokome Bentley <nokome@stenci.la>
2019-06-02 12:22:28 +1200

    fix(YAML): Skip invalid types e.g undefined when encoding
    This prevents an exception being raised if a property is or `undefined` or function etc. Consistent with `JSON.stringify` for `undefined`.

--- src/yaml.ts
+++ src/yaml.ts
@@ -10,9 +10,16 @@ import { dump, load, VFile } from './vfile'
 export const mediaTypes = ['text/yaml']
 
 export async function decode(file: VFile): Promise<stencila.Node> {
-  return coerce(yaml.safeLoad(await dump(file)))
+  const yml = await dump(file)
+  const node = yaml.safeLoad(yml)
+  return coerce(node)
 }
 
 export async function encode(node: stencila.Node): Promise<VFile> {
-  return load(yaml.safeDump(node))
+  const yml = yaml.safeDump(node, {
+    // "do not throw on invalid types (like function in the safe schema)
+    // and skip pairs and single values with such types."
+    skipInvalid: true
+  })
+  return load(yml)
 }
