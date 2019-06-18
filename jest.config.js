8cd272d0c5f3d1a56635406048c99b6584fb2d9d
Nokome Bentley <nokome@stenci.la>
2019-06-18 18:21:42 -0400

    chore(Package): Use @stencila/typescript-boilerplate

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.ts'],
-  setupFilesAfterEnv: ['<rootDir>/tests/matchers.ts'],
-  globalTeardown: '<rootDir>/tests/teardown.ts',
-  coveragePathIgnorePatterns: ['tests/helpers.ts', 'tests/fixtures/*'],
-  watchPathIgnorePatterns: ['tests/output']
-}
