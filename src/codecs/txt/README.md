4de9f20773827b059e998dd738e4c7a2d9e449e3
Nokome Bentley <nokome@stenci.la>
2019-07-01 11:28:14 +1200

    chore(TXT): Add docs and tests for `txt` codec

--- src/codecs/txt/README.md
+++ src/codecs/txt/README.md
@@ -1,3 +1,57 @@
-# TXT: Plain text
+# `txt`: Plain text codec
 
-This codec simply returns a string
+The `txt` codec is for decoding/encoding from/to plain text. It's main purpose is to provide a decoder for the `text/plain` media type (in particular for decoding outputs of Jupyter Notebook cells which have this media type). It may also be useful for decoding user input in plain text form fields.
+
+## Decoding
+
+The `txt` codec only decodes content to primitive node types i.e. `null`, `boolean`, `number` and `string`. It makes no attempt to decode more complex structure or semantics of the content.
+
+The string `"null"` is decoded to `null`, the string `"true"` and `"false"` are decoded to `boolean` values, and strings with only digits and an optional decimal place are decoded to a `number`.
+
+Everything else is decoded to a `string`. For example, this plain text content (with some Unicode characters),
+
+```txt import=ex1
+Yay, plain text! 👏
+```
+
+is decoded into a `string`, encoded in [JSON5](../json5) as,
+
+```json5 export=ex1
+'Yay, plain text! 👏'
+```
+
+## Encoding
+
+All `Node` types are encoded as plain text. For non-`string` primitive nodes, e.g. `boolean`, `number`, the plain text representation comes from the `toString()` method.
+
+No attempt is made to perform lossless encoding of more complex nodes. For `Array` nodes, items are encoded and joined by a single space. For `Object` nodes, key/value pairs are represented as space separated strings, with pairs joined by a space. For example, the following `Article` node, encoded as [JSON5](../json5),
+
+```json5 import=ex2
+{
+  type: 'Article',
+  title: 'My simple article',
+  authors: [
+    {
+      type: 'Person',
+      givenNames: ['Peter'],
+      familyNames: ['Pan']
+    }
+  ],
+  content: [
+    {
+      type: 'Paragraph',
+      content: ['My first paragraph.']
+    }
+  ]
+}
+```
+
+is encoded in `txt` as,
+
+```txt export=ex2
+type Article title My simple article authors type Person givenNames Peter familyNames Pan content type Paragraph content My first paragraph.
+```
+
+## Invertability
+
+The `txt` codec is only invertible for primitive node types. It is lossy for `array`, `object` and other more complex node types because it intentionally makes no attempt to encode these as plain text. Use one of the data transfer formats e.g. `json` or `yaml` for lossless text-based encoding.
---
+++ src/codecs/txt/txt.test.ts
@@ -0,0 +1,102 @@
+import stencila from '@stencila/schema'
+import path from 'path'
+import * as txt from '.'
+import { decode, encode } from '.'
+import { dump, load, read } from '../../util/vfile'
+
+describe('decode', () => {
+  const d = async (content: string) => await decode(await load(content))
+
+  it('decodes string representations of primitive nodes', async () => {
+    expect(await d('null')).toBe(null)
+    expect(await d('true')).toBe(true)
+    expect(await d('false')).toBe(false)
+    expect(await d('1')).toBe(1)
+    expect(await d('3.14')).toBe(3.14)
+    expect(await d(' 3.14 ')).toBe(' 3.14 ')
+    expect(await d('string')).toBe('string')
+  })
+
+  it('can decode a plain ASCII text file', async () => {
+    expect(
+      await decode(
+        await read(path.join(__dirname, '__fixtures__', 'ascii.txt'))
+      )
+    ).toBe('Just some plain ASCII text.\n')
+  })
+
+  it('can decode a UTF8 text file', async () => {
+    expect(
+      await decode(await read(path.join(__dirname, '__fixtures__', 'utf8.txt')))
+    ).toBe('ეს არის ქართულ ენაზე დაწერილი უნიკოდში.\n')
+  })
+})
+
+describe('encode', () => {
+  const e = async (node: stencila.Node) => await dump(await encode(node))
+
+  it('encodes primitives as strings', async () => {
+    expect(await e(null)).toBe('null')
+    expect(await e(true)).toBe('true')
+    expect(await e(false)).toBe('false')
+    expect(await e(1)).toBe('1')
+    expect(await e(3.14)).toBe('3.14')
+    expect(await e('string')).toBe('string')
+  })
+
+  it('encodes arrays as space separated strings', async () => {
+    expect(await e([])).toBe('')
+    expect(await e([1])).toBe('1')
+    expect(await e([null, true, 42, 'str', 3.14])).toBe('null true 42 str 3.14')
+  })
+
+  it('encodes objects as space separated key value pairs', async () => {
+    expect(await e({})).toBe('')
+    expect(await e({ a: 1 })).toBe('a 1')
+    expect(await e({ a: null, b: true, c: 42, d: 'str', e: 3.14 })).toBe(
+      'a null b true c 42 d str e 3.14'
+    )
+  })
+
+  it('encodes nested objects / arrays', async () => {
+    expect(await e(article)).toBe(articleString)
+  })
+})
+
+describe('invertibility', () => {
+  it('is able to invert primitive nodes', async () => {
+    expect(txt).toInvert(null)
+    expect(txt).toInvert(true)
+    expect(txt).toInvert(false)
+    expect(txt).toInvert(3.14)
+    expect(txt).toInvert(-100)
+    expect(txt).toInvert('3.14string')
+    expect(txt).toInvert('a string')
+  })
+
+  it('is not able to invert objects or arrays', async () => {
+    expect(txt).not.toInvert([1, 2, 3])
+    expect(txt).not.toInvert({ a: 1, b: 2 })
+  })
+})
+
+const article: stencila.Article = {
+  type: 'Article',
+  title: 'My simple article',
+  authors: [
+    {
+      type: 'Person',
+      givenNames: ['Peter'],
+      familyNames: ['Pan']
+    }
+  ],
+  content: [
+    {
+      type: 'Paragraph',
+      content: ['My first paragraph.']
+    }
+  ]
+}
+
+const articleString =
+  'type Article title My simple article authors type Person givenNames Peter familyNames Pan content type Paragraph content My first paragraph.'
