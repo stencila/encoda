a62f1e0f615297a15dd28cb558685a3a81347073
Nokome Bentley <nokome@stenci.la>
2019-07-01 18:25:11 +1200

    docs(*): Generate from top level of repo

--- docs.ts
+++ docs.ts
@@ -1,16 +1,16 @@
 /**
- * Generate documentation
- *
- *
+ * Generate documentation for Encoda using the `dir` codec
+ * to convert documentation files (matching patterns below)
+ * to a tree of HTML in `docs`.
  */
 
 import { decode, encode } from './src/codecs/dir'
 import * as vfile from './src/util/vfile'
-
 ;(async () => {
-  const docs = await decode(vfile.create('src'), {
+  const docs = await decode(vfile.create('.'), {
     patterns: [
-      '**/README.*',
+      '*.md',
+      'src/**/README.*',
       '!**/__fixtures__',
       '!**/__outputs__',
       '!**/__tests__'
