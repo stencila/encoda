5687f018383389dd586100fa0108fafdbc2a7ca3
Nokome Bentley <nokome@stenci.la>
2019-07-09 12:36:30 +1200

    test(ODT): Add note on why test failing

--- src/codecs/odt/odt.test.ts
+++ src/codecs/odt/odt.test.ts
@@ -2,5 +2,7 @@ import articleSimple from '../../__fixtures__/article-simple'
 import * as odt from './'
 
 test.skip('invertible', async () => {
+  // This currently fails because Pandoc does not recognise the
+  // `odt` Title paragraph style.
   await expect(odt).toInvert(articleSimple)
 })
