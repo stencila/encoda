8bfefadd1f765de0f6019fc4225b825a7b69470b
Nokome Bentley <nokome@stenci.la>
2019-07-11 14:42:56 +1200

    chore(Package): Add file snapshots and xml-js for JATS tests

--- src/__tests__/matchers.ts
+++ src/__tests__/matchers.ts
@@ -1,15 +1,23 @@
-import stencila from '@stencila/schema';
-import { nodeType } from '@stencila/schema/dist/util';
-import fs from 'fs-extra';
-import mime from 'mime';
-import path from 'path';
-import { Codec } from '..';
-
-// Ensure that the dir for test outputs is present
-fs.ensureDirSync(path.join(__dirname, '__outputs__'))
+import stencila from '@stencila/schema'
+import fs from 'fs-extra'
+import { toMatchFile } from 'jest-file-snapshot'
+import mime from 'mime'
+import path from 'path'
+import { Codec } from '..'
+import type from '../util/type'
 
 /**
- * Add a Jest matcher for testing that a codec is able
+ * Add https://github.com/satya164/jest-file-snapshot
+ *
+ * > Jest matcher to write snapshots to a separate file instead of the
+ * default snapshot file used by Jest. Writing a snapshot to a separate
+ * file means you have proper syntax highlighting in the output file,
+ * and better readability without those pesky escape characters.
+ */
+expect.extend({ toMatchFile })
+
+/**
+ * A Jest matcher for testing that a codec is able
  * to invert a node (ie. encode and then decode)
  * and produce useful error messages if it did not.
  *
@@ -17,45 +25,47 @@ fs.ensureDirSync(path.join(__dirname, '__outputs__'))
  * @param node The node to attempt to invert
  * @param name: The file name for any output files
  */
-expect.extend({
-  async toInvert(codec: Codec, node: stencila.Node, fileName?: string) {
-    if (!fileName) {
-      const typeName = nodeType(node).toLowerCase()
-      const num = Math.floor(Math.random() * Math.floor(1000))
-      fileName = `${typeName}-${num}`
-      const ext =
-        (codec.extNames && codec.extNames[0]) ||
-        (codec.mediaTypes && mime.getExtension(codec.mediaTypes[0]))
-      if (ext) fileName += '.' + ext
-    }
-    const outPath = path.join(__dirname, '__outputs__', fileName)
-    const file = await codec.encode(node, { filePath: outPath })
-    const nodeDecoded = await codec.decode(file)
-    try {
-      expect(nodeDecoded).toEqual(node)
-    } catch (error) {
-      return {
-        message: () => {
-          let extra
-          if (file.path)
-            extra = `\n\nthe generated file was: ${path.relative(
-              path.dirname(__dirname),
-              file.path
-            )}`
-          else extra = `\n\nthe generated content was: ${file.contents}`
-          return error.message + extra
-        },
-        pass: false
-      }
-    }
-    // Clean up
-    await fs.remove(outPath)
+async function toInvert(codec: Codec, node: stencila.Node, fileName?: string) {
+  if (!fileName) {
+    const typeName = type(node).toLowerCase()
+    const num = Math.floor(Math.random() * Math.floor(1000))
+    fileName = `${typeName}-${num}`
+    const ext =
+      (codec.extNames && codec.extNames[0]) ||
+      (codec.mediaTypes && mime.getExtension(codec.mediaTypes[0]))
+    if (ext) fileName += '.' + ext
+  }
+  const outPath = path.join(__dirname, '__outputs__', fileName)
+  await fs.ensureDir(path.dirname(outPath))
+  const file = await codec.encode(node, { filePath: outPath })
+  const nodeDecoded = await codec.decode(file)
+  try {
+    expect(nodeDecoded).toEqual(node)
+  } catch (error) {
     return {
-      message: () => 'ok!',
-      pass: true
+      message: () => {
+        let extra
+        if (file.path)
+          extra = `\n\nthe generated file was: ${path.relative(
+            path.dirname(__dirname),
+            file.path
+          )}`
+        else extra = `\n\nthe generated content was: ${file.contents}`
+        return error.message + extra
+      },
+      pass: false
     }
   }
-})
+  // Clean up
+  await fs.remove(outPath)
+  return {
+    message: () => 'ok!',
+    pass: true
+  }
+}
+
+expect.extend({ toInvert })
+
 declare global {
   namespace jest {
     interface Matchers<R> {
