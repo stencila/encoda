beb155dc6a7b89080e9b83e8d84ee46345f1b70a
Nokome Bentley <me@noko.me>
2019-07-15 17:54:56 +1200

    test(Logging): Force TTY output

--- src/__tests__/logger.ts
+++ src/__tests__/logger.ts
@@ -4,5 +4,6 @@ import { replaceHandlers, defaultHandler } from '@stencila/logga'
  * Show all log entries except for DEBUG level during tests
  */
 replaceHandlers(data => {
+  process.stderr.isTTY = true
   if (data.level < 3) defaultHandler(data)
 })
