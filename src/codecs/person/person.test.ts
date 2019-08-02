4937cfaddfa724f1530d0722bf333f4ba5ee4754
Nokome Bentley <nokome@stenci.la>
2019-08-02 16:47:46 +1200

    chore(Person): Bring over `person` codec from biblio

--- src/codecs/person/person.test.ts
+++ src/codecs/person/person.test.ts
@@ -1,5 +1,107 @@
-import { decode } from './'
+import { sniff, decodeSync, decode, encode } from './'
 import { coerce, create, validate } from '../../util/index'
+import { dump, load } from '../../util/vfile'
+
+test('sniff', async () => {
+  expect(await sniff('Joe Jones')).toBe(true)
+  expect(await sniff('J Jones')).toBe(true)
+  expect(await sniff('Joe James Jones')).toBe(true)
+  expect(await sniff('Joe J. Jones')).toBe(true)
+  expect(await sniff('Joe J. Jones <joe@example.com>')).toBe(true)
+  expect(await sniff('Joe J. Jones <joe-jones@example.com>')).toBe(true)
+
+  expect(await sniff('Joe')).toBe(false)
+  expect(await sniff('joe Jones')).toBe(false)
+  expect(await sniff('Lorem ipsum')).toBe(false)
+})
+
+describe('decodeSync', () => {
+  it('works', async () => {
+    let person = await create('Person')
+
+    person.familyNames = ['Jones']
+    expect(decodeSync('Jones')).toEqual(person)
+
+    person.givenNames = ['Jane', 'Jill']
+    expect(decodeSync('Jane Jill Jones')).toEqual(person)
+
+    person.honorificPrefix = 'Dr'
+    expect(decodeSync('Dr Jane Jill Jones')).toEqual(person)
+
+    person.honorificSuffix = 'PhD'
+    expect(decodeSync('Dr Jane Jill Jones PhD')).toEqual(person)
+
+    person.emails = ['jane@example.com']
+    expect(decodeSync('Dr Jane Jill Jones PhD <jane@example.com>')).toEqual(person)
+
+    person.url = 'http://example.com/jane'
+    expect(
+      decodeSync(
+        'Dr Jane Jill Jones PhD <jane@example.com> (http://example.com/jane)'
+      )
+    ).toEqual(person)
+  })
+
+  it('throws', () => {
+    expect(() => decodeSync('')).toThrow(
+      /^Unable to decode string \"\" as a person$/
+    )
+    expect(() => decodeSync('#@&%')).toThrow(
+      /^Unable to decode string \"#@&%\" as a person$/s
+    )
+  })
+})
+
+const joe = {
+  content: 'Joe Jones',
+  node: {
+    type: 'Person',
+    givenNames: ['Joe'],
+    familyNames: ['Jones']
+  }
+}
+
+const jane = {
+  content: 'Jane Jennifer Jones <jane@example.com>',
+  node: {
+    type: 'Person',
+    givenNames: ['Jane', 'Jennifer'],
+    familyNames: ['Jones'],
+    emails: ['jane@example.com']
+  }
+}
+
+const jill = {
+  content: 'Dr Jill J. Jones PhD <jill@example.com> (http://example.com/jill)',
+  node: {
+    type: 'Person',
+    givenNames: ['Jill', 'J.'],
+    familyNames: ['Jones'],
+    emails: ['jill@example.com'],
+    url: 'http://example.com/jill',
+    honorificPrefix: 'Dr',
+    honorificSuffix: 'PhD'
+  }
+}
+
+test('decode', async () => {
+  expect(await decode(load(joe.content))).toEqual(joe.node)
+  expect(await decode(load(jane.content))).toEqual(jane.node)
+  expect(await decode(load(jill.content))).toEqual(jill.node)
+
+  await expect(decode(load(''))).rejects.toThrow(
+    /^Unable to decode string \"\" as a person$/
+  )
+  await expect(decode(load('#@&%'))).rejects.toThrow(
+    /^Unable to decode string \"#@&%\" as a person$/
+  )
+})
+
+test('encode', async () => {
+  expect(await dump(await encode(joe.node))).toEqual(joe.content)
+  expect(await dump(await encode(jane.node))).toEqual(jane.content)
+  expect(await dump(await encode(jill.node))).toEqual(jill.content)
+})
 
 describe('validate', () => {
   it('throws for invalid emails', async () => {
@@ -12,43 +114,6 @@ describe('validate', () => {
         'Person'
       )
     ).rejects.toThrow('/emails/1: format should match format "email"')
-  })
-})
-
-describe('decode', () => {
-  it('works', async () => {
-    let person = await create('Person')
-
-    person.familyNames = ['Jones']
-    expect(decode('Jones')).toEqual(person)
-
-    person.givenNames = ['Jane', 'Jill']
-    expect(decode('Jane Jill Jones')).toEqual(person)
-
-    person.honorificPrefix = 'Dr'
-    expect(decode('Dr Jane Jill Jones')).toEqual(person)
-
-    person.honorificSuffix = 'PhD'
-    expect(decode('Dr Jane Jill Jones PhD')).toEqual(person)
-
-    person.emails = ['jane@example.com']
-    expect(decode('Dr Jane Jill Jones PhD <jane@example.com>')).toEqual(person)
-
-    person.url = 'http://example.com/jane'
-    expect(
-      decode(
-        'Dr Jane Jill Jones PhD <jane@example.com> (http://example.com/jane)'
-      )
-    ).toEqual(person)
-  })
-
-  it('throws', () => {
-    expect(() => decode('')).toThrow(
-      /^Unable to decode string \"\" as a person$/
-    )
-    expect(() => decode('#@&%')).toThrow(
-      /^Unable to decode string \"#@&%\" as a person$/s
-    )
   })
 })
 
