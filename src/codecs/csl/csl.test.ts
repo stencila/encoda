c36e8fc11be98aee4f7c9368ebc971334d057ada
Nokome Bentley <nokome@stenci.la>
2019-08-02 17:02:34 +1200

    chore(CSL): Bring over `csl` codec from biblio

---
+++ src/codecs/csl/csl.test.ts
@@ -0,0 +1,25 @@
+import { toMatchFile } from 'jest-file-snapshot';
+import path from 'path'
+import { decode, encode } from '.'
+import * as yaml from '../yaml'
+import * as vfile from '../../util/vfile'
+
+const fixture = (name: string) =>
+  path.join(__dirname, '__fixtures__', name)
+
+const snapshot = (name: string) =>
+  path.join(__dirname, '__file_snapshots__', name)
+
+test('decode', async () => {
+  const csl2yaml = async (name: string) =>
+    vfile.dump(await yaml.encode(await decode(await vfile.read(fixture(name)))))
+
+  expect(await csl2yaml('10.5334-jors-182.csl.json')).toMatchFile(snapshot('10.5334-jors-182.yaml'))
+})
+
+test('encode', async () => {
+  const yaml2csl = async (name: string) =>
+    vfile.dump(await encode(await yaml.decode(await vfile.read(fixture(name)))))
+
+  expect(await yaml2csl('article.yaml')).toMatchFile(snapshot('article.csl.json'))
+})
