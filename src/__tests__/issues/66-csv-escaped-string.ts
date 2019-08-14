f6c2f276681d0c4d7712d5b9ed04dcc9b7789687
Nokome Bentley <nokome@stenci.la>
2019-08-14 13:42:51 +1200

    tests(CSV): Document test result when removing leading spaces

--- src/__tests__/issues/66-csv-escaped-string.ts
+++ src/__tests__/issues/66-csv-escaped-string.ts
@@ -2,17 +2,22 @@ import { CSVCodec } from '../../codecs/csv'
 import * as vfile from '../../util/vfile'
 
 /**
- * The original issue way confusing a "parser" used in the `coerce()` function to
+ * The original issue was confusing a "parser" used in the `coerce()` function to
  * split comma separated lists (e.g. names of authors) with the decoding of csv files.
  *
- * At the time, the decoding of CSV files was being delegated to https://github.com/SheetJS/js-xlsx
- * which was presumed to handle the escaping of strings properly. However, this test showed
- * that there were deficiencies with `js-xlsx`'s parsing of CSV as noted below:
+ * At the time, the decoding of CSV files was being delegated to https://github.com/SheetJS/js-xlsx.
+ * Given the original test data in the issue, that seemed to have some problems with
+ * handling quoted values as documented here: https://github.com/stencila/encoda/commit/d12bc5a454034ba4b00c7c0fbd976cae9d49482b
+ *
+ * However, replacing `js-xlsx` with another CSV parser (`papaparse`) showed the same result
+ * and suggested that was expected behaviour given the trailing spaces after the commas in the
+ * test data. This commit, just shows that when removing those trailing spaces, you get
+ * expected behaviour.
  */
 test('issue 66: csv codec properly escapes strings', async () => {
   const {decode, encode} = new CSVCodec()
 
-  const csv = 'header1, header2, header3\ntext, "some text, it contains a comma", another text\n'
+  const csv = 'header1,header2,header3\ntext,"some text, it contains a comma",another text\n'
 
   const datatable = await decode(await vfile.load(csv))
   expect(datatable).toEqual({
@@ -27,25 +32,22 @@ test('issue 66: csv codec properly escapes strings', async () => {
           ],
         },
         {
-          "name": " header2", // Note leading space
+          "name": "header2",
           "type": "DatatableColumn",
           "values": [
-            " \"some text, it contains a comma\"",  // Note leading space and extra escaped quotes
+            "some text, it contains a comma",
           ],
         },
         {
-          "name": " header3", // Note leading space
+          "name": "header3",
           "type": "DatatableColumn",
           "values": [
-            " another text", // Note leading space
+            "another text",
           ],
         },
       ]
   })
 
   const csvOut = await vfile.dump(await encode(datatable))
-  expect(csvOut).toEqual(
-    // The extra quotes get put into the output, along with leading space
-    'header1, header2, header3\ntext," ""some text, it contains a comma""", another text\n'
-  )
+  expect(csvOut).toEqual(csv)
 })
