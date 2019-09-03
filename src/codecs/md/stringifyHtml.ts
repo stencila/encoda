45dd9ba69cb532158a179f37f36620568f543e8c
Alex Ketch <alex@ketch.me>
2019-09-03 16:11:04 -0400

    wip: checkpoint

--- src/codecs/md/stringifyHtml.ts
+++ src/codecs/md/stringifyHtml.ts
@@ -1,63 +1,116 @@
 import * as A from 'fp-ts/lib/Array'
 import * as O from 'fp-ts/lib/Option'
-import { Node, Parent } from 'unist'
+import { Node, Parent, Literal } from 'unist'
+import { eqString } from 'fp-ts/lib/Eq'
 
-const eatHtml = (closingIdx: number) => (hs: Node[]) =>
-  A.takeLeft(closingIdx)(hs).reduce(
-    (v: string, e) => v + (typeof e.value === 'string' ? e.value : ''),
+const selfClosingTags = [
+  'area',
+  'base',
+  'br',
+  'col',
+  'embed',
+  'hr',
+  'img',
+  'input',
+  'link',
+  'meta',
+  'param',
+  'source',
+  'track',
+  'wbr'
+]
+
+const parentNodeGuard = (n: Node): n is Parent => Array.isArray(n.children)
+
+const joinHTML = (tree: Node[]): string =>
+  tree.reduce(
+    (value: string, node: Node) =>
+      value + (typeof node.value === 'string' ? node.value : ''),
     ''
   )
 
-const parentGuard = (n: Node): n is Parent => Array.isArray(n.children)
+const htmlTagRegExp = new RegExp(/<\/?(\w+)[\s>]/g)
+const openingTagRegExp = new RegExp(/^<\w+/)
 
-export const stringifyHTML = (node: Node | Parent): Node => {
-  if (!parentGuard(node)) return node
+const isOpeningTag = (tag: string): boolean => openingTagRegExp.test(tag.trim())
 
-  let skipTill: number | undefined
+/**
+ * Returns a list of all opening and closing HTML tags. Note that this includs self-closing (void) elements.
+ * @param {string} html String representation of HTML to parse.
+ */
+const getTags = (html: string): string[] => html.match(htmlTagRegExp) || []
 
-  const ch = node.children.reduce((cs: Node[], c: Node, idx) => {
-    if (skipTill && idx <= skipTill) {
-      return cs
+/**
+ * stringifyHTML takes a UNIST (Remark) node tree and merges content between `html` node types for further processing by
+ * the Encoda HTML codec.
+ * This is necessary as the original tree has content split into HTML fragments such as
+ * `['<a href="#">', 'the inner content', '</a>']`, which the Codec does not know how to process.
+ * This functions transforms the above into `['<a href="#">the inner content</a>']`
+ * @param {(Node|Parent)} tree A UNIST Node which may or may not contain an array of child Nodes
+ */
+export const stringifyHTML = (tree: Node | Parent): Node => {
+  if (!parentNodeGuard(tree)) return tree
+
+  // This value allows us to skip over Nodes whose values have been merged into the new `fullHtml` node
+  let skipUntil: number | undefined
+
+  const fullHtmlNode = (value: string): Literal => ({
+    type: 'fullHtml',
+    value
+  })
+
+  const children = tree.children.reduce((innerTree: Node[], n: Node, idx) => {
+    if (skipUntil && idx <= skipUntil) {
+      return innerTree
     }
 
-    if (c.children) {
-      return [...cs, stringifyHTML(c)]
+    // Recursively call `stringifyHTML` if the Node has children of its own
+    if (n.children) {
+      return [...innerTree, stringifyHTML(n)]
     }
 
-    if (c.type === 'html' && typeof c.value === 'string') {
-      const _Tag = c.value.match(/<(\w+)/)
-      const selfClosing = c.value.includes('/>')
+    if (n.type === 'html' && typeof n.value === 'string') {
+      const openingTag = A.lookup(1, n.value.match(/<(\w+)/) || A.empty)
+      const isSelfClosing = A.elem(O.getEq(eqString))(
+        openingTag,
+        A.map(O.some)(selfClosingTags)
+      )
 
-      const openingTag = _Tag ? _Tag[1] : 0
+      // If the element is self-closing, then the the HTML value is already fully formed
+      if (isSelfClosing) {
+        return [...innerTree, fullHtmlNode(n.value)]
+      }
 
+      // Keep a stack counting number of opening HTML tags we've encountered
+      // This is used to avoid prematurely stopping gathering the HTML value, accounting for repeated nesting of the
+      // same HTML tags like `<div><div>content</div></div>`
       let stack = 0
-      const closing = A.findIndex((e: Node) => {
+      const closingTagIdx = A.findIndex((e: Node) => {
         if (typeof e.value !== 'string') return false
-        if (selfClosing) return true
 
-        if (e.value.includes(`<${openingTag}`)) {
-          stack++
-        } else if (e.value.includes(`</${openingTag}`)) {
-          stack--
-        }
+        // Split HTML tags into two lists, one for opening and one for closings tags. We use the difference in their
+        // lengths to determine if there are any unclosed HTML tags, and if we should continue consuming the value of
+        // the next Node.
+        const groupedTags = A.partition(isOpeningTag)(getTags(e.value))
+        stack += groupedTags.right.length - groupedTags.left.length
 
-        return stack === 0 && e.value.includes(`</${openingTag}`)
-      })(node.children)
+        return stack === 0
+      })(tree.children)
 
-      const closingIdx = O.getOrElse<number>(() => 0)(closing) + 1
-      skipTill = closingIdx
+      // If we couldn't find the ending closing tag in this Node, don't skip any Nodes
+      skipUntil = O.getOrElse<number>(() => idx)(closingTagIdx)
 
-      return [
-        ...cs,
-        {
-          type: 'fullHtml',
-          value: eatHtml(closingIdx)(A.dropLeft(idx)(node.children))
-        }
-      ]
+      // Now that we know where in the list of the children the HTML starts and stops, we can merge their values
+      const subSet = tree.children.slice(idx, skipUntil + 1)
+
+      return [...innerTree, fullHtmlNode(joinHTML(subSet))]
     }
 
-    return [...cs, c]
+    return [...innerTree, n]
   }, [])
 
-  return { ...node, children: ch }
+  // Reset the skipUntil counter
+  skipUntil = undefined
+
+  return { ...tree, children }
 }
