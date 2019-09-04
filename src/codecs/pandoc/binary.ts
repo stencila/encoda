2ffa639efe9879d4943c8ff246e828e7e4d836c0
Nokome Bentley <nokome@stenci.la>
2019-09-04 16:49:43 +1200

    test(Pandoc): Fix paths and test

--- src/codecs/pandoc/binary.ts
+++ src/codecs/pandoc/binary.ts
@@ -13,12 +13,12 @@ import isPackaged from '../../util/app/isPackaged'
 
 const version = '2.7.3'
 const base = `https://github.com/jgm/pandoc/releases/download/${version}/pandoc-${version}`
+
+const isCompiled = __filename.endsWith('.js')
+
 const home = path.join(
   appHome,
-  ...(isPackaged ? ['node_modules', '@stencila', 'encoda'] : []),
-  'dist',
-  'codecs',
-  'pandoc'
+  ...(isPackaged ? ['node_modules', '@stencila', 'encoda'] : [])
 )
 
 export const binary = new BinWrapper()
@@ -26,7 +26,7 @@ export const binary = new BinWrapper()
   .src(`${base}-macOS.zip`, 'darwin')
   .src(`${base}-windows-i386.zip`, 'win32', 'x32')
   .src(`${base}-windows-x86_64.zip`, 'win32', 'x64')
-  .dest(path.join(home, 'binary'))
+  .dest(path.join(home, 'dist', 'codecs', 'pandoc', 'binary'))
   .use(process.platform === 'win32' ? 'pandoc.exe' : 'bin/pandoc')
   .version(version)
 
@@ -34,7 +34,7 @@ export const binary = new BinWrapper()
  * Equivalent to the Pandoc `--data-dir` flag.
  * Instructs Pandoc where to find templates and other assets.
  */
-export const dataDir = home
+export const dataDir = path.join(home, isCompiled ? 'dist' : 'src', 'codecs', 'pandoc')
 
 /**
  * Install the Pandoc binary if it is not already
