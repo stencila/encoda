c9d345b21cc645e362133b39ef42a4ee7303160b
Nokome Bentley <nokome@stenci.la>
2019-09-25 12:09:58 +1200

    feat(Util): Add `transformSync` function

--- src/util/transform.ts
+++ src/util/transform.ts
@@ -7,7 +7,7 @@ import produce from 'immer'
 
 /**
  * Transform a `Node` by applying a transformer function to
- * each node an its children.
+ * each node and its children.
  *
  * @param node The node to transform
  * @param transformer The transforming function. Should return the transformed node.
@@ -28,8 +28,35 @@ export default async function transform(
       // @ts-ignore
       transformed[key] = await walk(child, [...path, key])
     }
-
     return transformed
   }
   return produce(node, walk)
+}
+
+/**
+ * Synchronous version of `transform`.
+ */
+export function transformSync(
+  node: stencila.Node,
+  transformer: (node: stencila.Node, path?: string[]) => stencila.Node
+): stencila.Node {
+  function walk(node: stencila.Node, path: string[] = []): stencila.Node {
+    const transformed = transformer(node, path)
+    if (stencila.isPrimitive(transformed) || transformed === undefined) {
+      return transformed
+    }
+    if (Array.isArray(transformed)) {
+      return transformed.map((child, index) =>
+        walk(child, [...path, `${index}`])
+      )
+    }
+    return Object.entries(transformed).reduce(
+      (prev, [key, child]) => ({
+        ...prev,
+        ...{ [key]: walk(child, [...path, key]) }
+      }),
+      {}
+    )
+  }
+  return walk(node)
 }
