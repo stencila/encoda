e4c0522047c58ed5fa5ffc055e1fcdbed4035fbf
Nokome Bentley <nokome@stenci.la>
2019-09-26 16:11:47 +1200

    fix(YAML): Order properties when encoding

--- src/codecs/yaml/yaml.test.ts
+++ src/codecs/yaml/yaml.test.ts
@@ -1,6 +1,27 @@
 import articleSimple from '../../__fixtures__/article-simple'
 import { YamlCodec } from './'
 
+const yamlCodec = new YamlCodec()
+
+describe('encode', () => {
+  test('property ordering', async () =>
+    expect(
+      await yamlCodec.dump({
+        depth: 1,
+        content: ['Heading number one'],
+        type: 'Heading',
+        id: 'heading-1'
+      })
+    ).toEqual(
+      `type: Heading
+id: heading-1
+depth: 1
+content:
+  - Heading number one
+`
+    ))
+})
+
 test('invertible', async () => {
-  await expect(new YamlCodec()).toInvert(articleSimple)
+  await expect(yamlCodec).toInvert(articleSimple)
 })
