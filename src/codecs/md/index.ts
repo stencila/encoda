7d158064581ad2aa149b260196f58e8eddec1e4b
alex-ketch <alex@ketch.me>
2020-01-22 17:25:38 -0500

    fix(MD): Don't coerce decoded MD AST, avoiding false null values

--- src/codecs/md/index.ts
+++ src/codecs/md/index.ts
@@ -5,11 +5,11 @@
 import { getLogger } from '@stencila/logga'
 import stencila, {
   isBlockContent,
+  isInlineContent,
   isListItem,
   nodeIs,
   nodeType,
-  TypeMapGeneric,
-  isInlineContent
+  TypeMapGeneric
 } from '@stencila/schema'
 import * as yaml from 'js-yaml'
 import JSON5 from 'json5'
@@ -36,12 +36,11 @@ import filter from 'unist-util-filter'
 import map from 'unist-util-map'
 // @ts-ignore
 import { selectAll } from 'unist-util-select'
-import * as vfile from '../../util/vfile'
 import { stringifyContent } from '../../util/content/stringifyContent'
-import { Codec } from '../types'
+import * as vfile from '../../util/vfile'
 import { HTMLCodec } from '../html'
+import { Codec } from '../types'
 import { stringifyHTML } from './stringifyHtml'
-import { coerce } from '../../util/coerce'
 
 export const log = getLogger('encoda:md')
 
@@ -58,8 +57,7 @@ export class MdCodec extends Codec implements Codec {
     file: vfile.VFile
   ): Promise<stencila.Node> => {
     const md = await vfile.dump(file)
-    const node = decodeMarkdown(md)
-    return coerce(node)
+    return decodeMarkdown(md)
   }
 
   /**
