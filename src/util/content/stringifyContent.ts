d02815297258b28bced09db6213372fd7335c8dc
Nokome Bentley <nokome@stenci.la>
2020-02-12 19:05:20 +1300

    fix(stringifyContent): Reduce noise
    Closes #428.

--- src/util/content/stringifyContent.ts
+++ src/util/content/stringifyContent.ts
@@ -1,7 +1,4 @@
 import stencila from '@stencila/schema'
-import { getLogger } from '@stencila/logga'
-
-const log = getLogger('encoda:stringify')
 
 /**
  * Stringify a node or its `content`.
@@ -15,26 +12,9 @@ export const stringifyContent = (node: stencila.Node): string => {
   if (node === undefined) return ''
   if (node === null) return 'null'
   if (Array.isArray(node)) return node.map(stringifyContent).join('')
-  if (typeof node === 'object') {
-    if ('content' in node) {
-      const { content, ...rest } = node
-      const properties = Object.keys(rest)
-      if (properties.length > 0) {
-        log.warn(
-          `Node properties ignored when stringifying: ${properties
-            .map(prop => `\`${prop}\``)
-            .join(', ')}`
-        )
-      }
-      return stringifyContent(content)
-    } else {
-      log.warn(
-        `Node keys ignored when stringifying: ${Object.keys(node)
-          .map(key => `\`${key}\``)
-          .join(', ')}`
-      )
-      return stringifyContent(Object.values(node))
-    }
-  }
+  if (typeof node === 'object')
+    return stringifyContent(
+      'content' in node ? node.content : Object.values(node)
+    )
   return node.toString()
 }
