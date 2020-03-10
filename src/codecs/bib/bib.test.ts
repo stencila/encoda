bb80d4e35e9c41b01c3c6cbc6f1fd3da406677c1
Nokome Bentley <nokome@stenci.la>
2020-03-11 09:08:44 +1300

    test(*): Skip more tests on Windows CI

--- src/codecs/bib/bib.test.ts
+++ src/codecs/bib/bib.test.ts
@@ -7,7 +7,7 @@ const yamlCodec = new YamlCodec()
 
 // Skip on Windows CI due to these errors:
 // https://dev.azure.com/stencila/stencila/_build/results?buildId=719&view=logs&j=b17395f6-68a3-5682-0476-d3f6f1043109&t=0d6dcb53-0957-53aa-c18a-237166af945a&l=443
-if (!(process.env.CI && process.platform === 'win32'))
+if (!(process.env.CI && process.platform === 'win32')) {
   test('decode', async () => {
     const bib2yaml = async (name: string) =>
       yamlCodec.dump(await bibCodec.read(fixture(name)))
@@ -15,9 +15,10 @@ if (!(process.env.CI && process.platform === 'win32'))
     expect(await bib2yaml('small.bib')).toMatchFile(snapshot('small.yaml'))
   })
 
-test('encode', async () => {
-  const yaml2bib = async (name: string) =>
-    bibCodec.dump(await yamlCodec.read(fixture(name)))
+  test('encode', async () => {
+    const yaml2bib = async (name: string) =>
+      bibCodec.dump(await yamlCodec.read(fixture(name)))
 
-  expect(await yaml2bib('article.yaml')).toMatchFile(snapshot('article.bib'))
-})
+    expect(await yaml2bib('article.yaml')).toMatchFile(snapshot('article.bib'))
+  })
+}
--- src/codecs/csl/csl.test.ts
+++ src/codecs/csl/csl.test.ts
@@ -7,7 +7,7 @@ const cslCodec = new CSLCodec()
 
 // Skip on Windows CI due to these errors:
 // https://dev.azure.com/stencila/stencila/_build/results?buildId=719&view=logs&j=b17395f6-68a3-5682-0476-d3f6f1043109&t=0d6dcb53-0957-53aa-c18a-237166af945a&l=443
-if (!(process.env.CI && process.platform === 'win32'))
+if (!(process.env.CI && process.platform === 'win32')) {
   test('decode', async () => {
     const csl2yaml = async (name: string) =>
       yamlCodec.dump(await cslCodec.read(fixture(name)))
@@ -17,11 +17,12 @@ if (!(process.env.CI && process.platform === 'win32'))
     )
   })
 
-test('encode', async () => {
-  const yaml2csl = async (name: string) =>
-    cslCodec.dump(await yamlCodec.read(fixture(name)))
+  test('encode', async () => {
+    const yaml2csl = async (name: string) =>
+      cslCodec.dump(await yamlCodec.read(fixture(name)))
 
-  expect(await yaml2csl('article.yaml')).toMatchFile(
-    snapshot('article.csl.json')
-  )
-})
+    expect(await yaml2csl('article.yaml')).toMatchFile(
+      snapshot('article.csl.json')
+    )
+  })
+}
