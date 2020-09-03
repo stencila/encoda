321687415e41f17a1ec6b8e3d6ef0b1bb654d5e7
Nokome Bentley <nokome@stenci.la>
2020-09-04 10:56:58 +1200

    fix(MediaObjects): Fix resolution of files
    Previous implementation did not work when converting a file that was not in the current working directory.

--- src/util/media/resolveFiles.ts
+++ src/util/media/resolveFiles.ts
@@ -3,16 +3,20 @@
  */
 
 import * as schema from '@stencila/schema'
-import { transformSync } from '../transform'
+import fs from 'fs'
 import path from 'path'
+import { transformSync } from '../transform'
 
 /**
- * Transform a `Node` by resolving links to local media resources.
+ * Transform a `Node` by resolving all links to local media resources.
  *
- * This is used to create documents that can be converted to other formats,
- * in other locations, while not breaking those links
+ * Any `contentUrl` of `MediaObject` nodes that point to files
+ * are converted to absolute paths for their in memory representation.
+ * Later, when encoding to another format, these paths are are made relative
+ * by the `toFiles` function.
  *
  * @see unlinkFiles
+ * @see toFiles
  */
 export function resolveFiles(node: schema.Node, docPath: string): schema.Node {
   return transformSync(
@@ -21,9 +25,21 @@ export function resolveFiles(node: schema.Node, docPath: string): schema.Node {
       if (schema.nodeIs(schema.mediaObjectTypes)(node)) {
         const { contentUrl, ...rest } = node
         if (!contentUrl.startsWith('http') && !contentUrl.startsWith('data:')) {
+          let resolvedPath
+          if (path.isAbsolute(contentUrl)) {
+            // Already absolute, no need to do anything
+            resolvedPath = contentUrl
+          } else if (fs.existsSync(contentUrl)) {
+            // Path is relative to the cwd, so make it absolute
+            resolvedPath = path.resolve(contentUrl)
+          } else {
+            // Path could be relative to the document, so try that
+            // as a last resort
+            resolvedPath = path.resolve(path.dirname(docPath), contentUrl)
+          }
           return {
             ...rest,
-            contentUrl: path.resolve(path.dirname(docPath), contentUrl),
+            contentUrl: resolvedPath,
           }
         }
       }
