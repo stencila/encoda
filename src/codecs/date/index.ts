f85eab6024a1f347daaf935edd7b89ab2eff3c6b
Nokome Bentley <nokome@stenci.la>
2020-09-17 14:01:04 +1200

    fix(Date): Allow date values to be short ISO strings e.g 2009-01

--- src/codecs/date/index.ts
+++ src/codecs/date/index.ts
@@ -14,6 +14,23 @@ export class DateCodec extends Codec implements Codec {
     file: vfile.VFile
   ): Promise<stencila.Node> => {
     const content = await vfile.dump(file)
+
+    // If the content is already valid ISO 8601 then just return it
+    // as the value of the date. This avoids having to parse the date
+    // and then generating a concise ISO 8601 string e.g. for 2020-09
+    // This regex balances permissiveness with
+    // complexity. More complex, less permissive regexes for this exist
+    // (see https://github.com/hapi-server/data-specification/issues/54)
+    // but are probably unnecessary for this use case.
+    if (
+      /^(-?(?:[1-9][0-9]*)?[0-9]{4})(-(1[0-2]|0[1-9]))?(-(3[01]|0[1-9]|[12][0-9]))?(T(2[0-3]|[01][0-9]))?(:[0-5][0-9])?(:[0-5][0-9])?(\.[0-9]+)?Z?$/.test(
+        content
+      )
+    ) {
+      return stencila.date({ value: content })
+    }
+
+    // Date needs parsing
     // Add UTC to force parsing as UTC, rather than local.
     let date = new Date(content + ' UTC')
     // But if that fails, because another timezone specified then
@@ -23,13 +40,19 @@ export class DateCodec extends Codec implements Codec {
       log.warn(`Unable to decode content to date: "${content}"`)
       return stencila.date({ value: '' })
     }
-    const iso = date.toISOString()
-    return stencila.date({ value: iso })
+
+    // After parsing the date shorten it a much as possible
+    // Assumes that it the user wanted to specify a date/time as precisely
+    // being midnight UTC that they would enter it as an ISO string in the
+    // first place
+    let value = date.toISOString()
+    if (value.endsWith('T00:00:00.000Z')) value = value.substring(0, 10)
+
+    return stencila.date({ value })
   }
 
   public readonly encode = (node: stencila.Node): Promise<vfile.VFile> => {
     let iso = stencila.isA('Date', node) ? node.value : ''
-    if (iso.endsWith('T00:00:00.000Z')) iso = iso.substring(0, 10)
     return Promise.resolve(vfile.load(iso))
   }
 }
