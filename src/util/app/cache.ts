1262f6bdcad04c1c3495f03172673fd80940d7b8
Nokome Bentley <nokome@stenci.la>
2020-11-25 09:55:06 +1300

    fix(Cache): Remove unused async version; use temp dir

--- src/util/app/cache.ts
+++ src/util/app/cache.ts
@@ -1,97 +1,77 @@
-/**
- * A file cache based on [Keyv](https://github.com/lukechilds/keyv)
- * allowing for TTL (time to live) based expiry.
- *
- * Used by the `util/http` module for caching HTTP requests.
- *
- * @module util/app/cache
- */
-
-import AsyncLock from 'async-lock'
+import crypto from 'crypto'
 import fs from 'fs-extra'
 import Keyv from 'keyv'
 import path from 'path'
-import data from './data'
-
-/**
- * Lock to prevent races among
- * asynchronous calls. e.g. `get` before
- * `set` has finished writing file.
- */
-const lock = new AsyncLock()
+import tempy from 'tempy'
 
 /**
  * Cache class that implements the methods required to
  * act as a Keyv storage adapter.
+ *
+ * For a previous attempt an an async store see
+ * https://github.com/stencila/encoda/blob/bbad58784e7af4d64fca73eb02568e750f5bbcf8/src/util/app/cache.ts
  */
 export class Cache implements Keyv.Store<string> {
   /**
    * The directory for the cache files.
-   * Defaults to `cache` in the this apps data directory.
    */
   private dir: string
 
-  public constructor(dir: string = path.join(data, 'cache')) {
-    this.dir = dir
+  public constructor() {
+    this.dir = tempy.directory({ prefix: 'encoda' })
+    fs.ensureDirSync(this.dir)
   }
 
   /**
-   * Generates a valid file name, within the cache directory,
-   * which excludes any characters that are invalid in file system paths.
+   * Generates a file name within the cache directory.
+   *
+   * Use a hash to avoid invalid characters and names
+   * that are too long. Use SHA1 because faster than SHA256
+   * and does not need to be secure.
    */
   private filename(key: string): string {
-    const filename = key.replace(/[<>:"/\\|?*]/g, '!')
+    const filename = crypto.createHash('sha1').update(key).digest('hex')
     return path.join(this.dir, filename)
   }
 
   /**
    * Set a value to be cached.
    */
-  public async set(key: string, value: string): Promise<void> {
-    await lock.acquire('dir', async () => fs.ensureDir(this.dir))
-
-    const filename = this.filename(key)
-    return lock.acquire(filename, async () => {
-      await fs.writeFile(filename, value, 'utf8')
-    })
+  public set(key: string, value: string): void {
+    fs.writeFileSync(this.filename(key), value, 'utf8')
   }
 
   /**
    * Get a value from the cache.
    */
-  public async get(key: string): Promise<string | undefined> {
-    const filename = this.filename(key)
-    return lock.acquire(filename, async () => {
-      try {
-        const value = await fs.readFile(filename, 'utf8')
-        return value
-      } catch (error) {
-        if (error.code === 'ENOENT') return undefined
-        throw error
-      }
-    })
+  public get(key: string): string | undefined {
+    try {
+      return fs.readFileSync(this.filename(key), 'utf8')
+    } catch (error) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      if (error.code === 'ENOENT') return undefined
+      throw error
+    }
   }
 
   /**
    * Delete a value from the cache.
    */
-  public async delete(key: string): Promise<boolean> {
-    const filename = this.filename(key)
-    return lock.acquire(filename, async () => {
-      try {
-        await fs.unlink(filename)
-      } catch (error) {
-        if (error.code !== 'ENOENT') throw error
-      }
-      return true
-    })
+  public delete(key: string): boolean {
+    try {
+      fs.unlinkSync(this.filename(key))
+    } catch (error) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      if (error.code !== 'ENOENT') throw error
+    }
+    return true
   }
 
   /**
    * Clear the cache completely.
    */
-  public async clear(): Promise<void> {
-    return lock.acquire('dir', async () => fs.remove(this.dir))
+  public clear(): void {
+    return fs.removeSync(this.dir)
   }
 }
 
