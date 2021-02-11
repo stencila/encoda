27c69c03253b255d33c42f312986ba1e4359d490
Nokome Bentley <nokome@stenci.la>
2021-02-12 09:44:27 +1300

    test(Timeouts & Nock mode): Increase timeouts on CI; reduce locally; turn back on wild

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -1,9 +1,6 @@
 import * as log from '../log'
 import { shutdown } from '../'
 
-// Many tests take longer than the default 5s timeout
-jest.setTimeout(5 * 60 * 1000)
-
 // Force TTY style output for better log readability
 process.stderr.isTTY = true
 
