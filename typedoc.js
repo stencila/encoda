11360edc58fdcff4b8e858a36764e24fdbc596e1
Nokome Bentley <nokome@stenci.la>
2021-02-12 12:42:46 +1300

    docs(API): Fix typedoc config

--- typedoc.js
+++ typedoc.js
@@ -1,9 +1,11 @@
 module.exports = {
+  tsconfig: 'tsconfig.prod.json',
   exclude: [
     '**/node_modules/**/*',
-    '**/__tests__/**/*',
     '**/__fixtures__/**/*',
+    '**/__file_snapshots__/**/*',
     '**/__mocks__/**/*',
+    '**/__tests__/**/*',
     '**/**/*.test.ts',
   ],
   out: './docs/api',
