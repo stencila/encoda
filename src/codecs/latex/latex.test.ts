24e895eb68f505d75e7ce52d8fd2e8522284f3d1
Nokome Bentley <nokome@stenci.la>
2021-03-15 14:27:01 +1300

    test(Latex): Added test fixture

--- src/codecs/latex/latex.test.ts
+++ src/codecs/latex/latex.test.ts
@@ -1,8 +1,21 @@
 import articleSimple from '../../__fixtures__/article/simple'
+import { fixture, snapshot } from '../../__tests__/helpers'
+import { JsonCodec } from '../json'
 import { LatexCodec } from './'
 
-const latex = new LatexCodec()
+const jsonCodec = new JsonCodec()
+const latexCodec = new LatexCodec()
 
 test('invertible', async () => {
-  await expect(latex).toInvert(articleSimple)
+  await expect(latexCodec).toInvert(articleSimple)
+})
+
+describe('fixtures', () => {
+  test('test1.tex', async () => {
+    const test1 = await latexCodec.read(fixture('test1.tex'), {
+      shouldCoerce: false,
+      shouldReshape: false,
+    })
+    expect(await jsonCodec.dump(test1)).toMatchFile(snapshot('test1.json'))
+  })
 })
