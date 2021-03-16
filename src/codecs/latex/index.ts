f782dd1c66518b3c11fdfb3de6af9067ae9de086
Nokome Bentley <nokome@stenci.la>
2021-03-17 09:30:54 +1300

    fix(LaTeX): Extract equation label to MathBlock id

--- src/codecs/latex/index.ts
+++ src/codecs/latex/index.ts
@@ -2,7 +2,8 @@
  * @module latex
  */
 
-import stencila from '@stencila/schema'
+import schema from '@stencila/schema'
+import { transformSync } from '../../util/transform'
 import * as vfile from '../../util/vfile'
 import { InputFormat, OutputFormat, PandocCodec } from '../pandoc'
 import { Codec, CommonDecodeOptions, CommonEncodeOptions } from '../types'
@@ -17,18 +18,42 @@ export class LatexCodec extends Codec implements Codec {
   public readonly decode = async (
     file: vfile.VFile,
     options: CommonDecodeOptions = this.commonDecodeDefaults
-  ): Promise<stencila.Node> => {
-    return pandoc.decode(file, options, {
+  ): Promise<schema.Node> => {
+    const root = await pandoc.decode(file, options, {
       pandocFormat: InputFormat.latex,
+    })
+    return transformSync(root, (node) => {
+      if (schema.isA('MathBlock', node)) return decodeMathBlock(node)
+      return node
     })
   }
 
   public readonly encode = async (
-    node: stencila.Node,
+    node: schema.Node,
     options: CommonEncodeOptions = this.commonEncodeDefaults
   ): Promise<vfile.VFile> => {
     return pandoc.encode(node, options, {
       pandocFormat: OutputFormat.latex,
     })
   }
+}
+
+/**
+ * Decode a MathBlock
+ *
+ * This function is necessary because Pandoc does not parse the
+ * `\label{}` of an equation (as it does for tables and figures).
+ * This provides consistency by extracting the label into the `id`
+ * property (Latex's \label actually reflects the semantics of
+ * Stencila schema `id` better than the `label` property).
+ */
+function decodeMathBlock(node: schema.MathBlock): schema.MathBlock {
+  let { text } = node
+  const match = /\\label{(.*?)}/.exec(text)
+  if (match) {
+    const id = match[1]
+    text = text.replace(match[0], '').trim()
+    return { ...node, id, text }
+  }
+  return node
 }
--- src/codecs/latex/latex.test.ts
+++ src/codecs/latex/latex.test.ts
@@ -1,4 +1,4 @@
-import { article, paragraph } from '@stencila/schema'
+import { article, mathBlock, paragraph } from '@stencila/schema'
 import articleSimple from '../../__fixtures__/article/simple'
 import { fixture, snapshot } from '../../__tests__/helpers'
 import { JsonCodec } from '../json'
@@ -9,6 +9,16 @@ const latexCodec = new LatexCodec()
 
 test('invertible', async () => {
   await expect(latexCodec).toInvert(articleSimple)
+})
+
+describe('fixtures', () => {
+  test('test1.tex', async () => {
+    const test1 = await latexCodec.read(fixture('test1.tex'), {
+      shouldCoerce: false,
+      shouldReshape: false,
+    })
+    expect(await jsonCodec.dump(test1)).toMatchFile(snapshot('test1.json'))
+  })
 })
 
 test('decoding multiple lines of text', async () => {
@@ -30,12 +40,20 @@ And we want to have a space after that last period.
   )
 })
 
-describe('fixtures', () => {
-  test('test1.tex', async () => {
-    const test1 = await latexCodec.read(fixture('test1.tex'), {
-      shouldCoerce: false,
-      shouldReshape: false,
+test('decode equation with label to MathBlock with id (as for Table and Figure)', async () => {
+  const node = await latexCodec.load(`
+\\begin{equation}
+  \\text{E}[x] = \\frac{1.0}{1.0}
+  \\label{eq:eq1}
+\\end{equation}`)
+  expect(node).toEqual(
+    article({
+      content: [
+        mathBlock({
+          text: `\\text{E}[x] = \\frac{1.0}{1.0}`,
+          id: 'eq:eq1',
+        }),
+      ],
     })
-    expect(await jsonCodec.dump(test1)).toMatchFile(snapshot('test1.json'))
-  })
+  )
 })
