8e53b590a0f9fb3620974e7a8b366c5afed896cb
Nokome Bentley <nokome@stenci.la>
2021-03-23 18:40:06 +1300

    feat(Noweb): Parse noweb style code chunks

--- src/codecs/nw/index.ts
+++ src/codecs/nw/index.ts
@@ -3,7 +3,7 @@ import * as vfile from '../../util/vfile'
 import { LatexCodec } from '../latex'
 import { Codec, CommonDecodeOptions, CommonEncodeOptions } from '../types'
 
-const latex = new LatexCodec()
+const latexCodec = new LatexCodec()
 
 export class NwCodec extends Codec implements Codec {
   public readonly mediaTypes = ['application/x-nw']
@@ -14,13 +14,33 @@ export class NwCodec extends Codec implements Codec {
     file: vfile.VFile,
     options: CommonDecodeOptions = this.commonDecodeDefaults
   ): Promise<schema.Node> => {
-    return latex.decode(file, options)
+    const nw = await vfile.dump(file)
+    const latex = decodeCodeExpression(decodeCodeChunk(nw))
+    return latexCodec.decode(vfile.load(latex), options)
   }
 
   public readonly encode = async (
     node: schema.Node,
     options: CommonEncodeOptions = this.commonEncodeDefaults
   ): Promise<vfile.VFile> => {
-    return latex.encode(node, options)
+    return latexCodec.encode(node, options)
   }
+}
+
+export function decodeCodeExpression(nw: string): string {
+  // TODO
+  return nw
+}
+
+// See https://regex101.com/r/MByuPq/1
+export function decodeCodeChunk(nw: string): string {
+  // TODO Use the correct language
+  return nw.replace(
+    /<<(.*?)>>=((.|\r|\n)*?)\r?\n@\n/g,
+    (_match, label, text) => `\\begin{lstlisting}[language=r exec${
+      label !== undefined ? `, id=${label}` : ''
+    }]
+${text}
+\\end{lstlisting}`
+  )
 }
