c0517d408f07db9cc62f79310eaaf5342da14649
Nokome Bentley <nokome@stenci.la>
2021-06-21 18:52:04 +1200

    fix(Build): Do not use compression withing binary

--- build.sh
+++ build.sh
@@ -3,7 +3,14 @@
 set -e
 
 # Build binaries (see `pkg` in `package.json` for details)
-npx pkg --out-path bin --compress Brotli .
+# The `--compress Gzip` option reduced binary size from ~340Mb
+# to ~146Mb but the `x86_64-unknown-linux-gnu.tar.gz` etc files
+# was actually slightly larger. Brotli compression does not
+# offer much further advantage and is verrrry slow to compress.
+# Given that the `--compress` option can reduce startup times
+# and offers little / no advantage for download sizes it is
+# not enabled.
+npx pkg --out-path bin .
 
 # Zip binaries with renaming appropriate to the platform
 cd bin
