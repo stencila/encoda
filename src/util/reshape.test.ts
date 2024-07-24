bbec2a914b37580412bdf445264d1d5a86f767b2
Will Byrne <will.alex.byrne@gmail.com>
2024-07-24 17:51:30 +0100

    Remove md test from reshape.test.ts and remove its related fixtures and files

--- src/util/reshape.test.ts
+++ src/util/reshape.test.ts
@@ -1,28 +1,7 @@
 import schema from '@stencila/schema'
 
-import { MdCodec } from '../codecs/md'
-import { YamlCodec } from '../codecs/yaml'
-import { fixture, nockRecord, snapshot } from '../__tests__/helpers'
 import { groupCitesIntoCiteGroup } from './reshape'
 
-const mdCodec = new MdCodec()
-const yamlCodec = new YamlCodec()
-
-test('reshape', async () => {
-  const done = await nockRecord('nock-record-reshape.json')
-
-  const reshaped = await mdCodec.read(fixture('reshape-1.md'), {
-    shouldReshape: true,
-  })
-  expect(await yamlCodec.dump(reshaped)).toMatchFile(
-    snapshot('reshape-1-reshaped.yaml')
-  )
-  expect(await mdCodec.dump(reshaped)).toMatchFile(
-    snapshot('reshape-1-reshaped.md')
-  )
-
-  done()
-})
 
 describe('groupCitesIntoGiteGroup', () => {
   test('single parenthetical Cite has extraneous parentheses removed', () => {
