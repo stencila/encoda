9e53886d085997770b2f5a4142adaec8f91a5549
Nokome Bentley <nokome@stenci.la>
2024-10-16 20:25:00 +1300

    chore(*): remove `util/xslt` and `util/puppeteer`

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -1,5 +1,4 @@
 import * as log from '../util/logging'
-import { shutdown } from '../'
 
 // Force TTY style output for better log readability
 process.stderr.isTTY = true
@@ -8,11 +7,3 @@ process.stderr.isTTY = true
 //   DEBUG=1 npm test
 // Do not exit on errors
 log.configure(process.env.DEBUG !== undefined, false)
-
-// After all tests have finished run the shutdown
-// function. This needs to be done here, rather
-// than in a global teardown script because
-// tests are run in separate processes
-afterAll(async () => {
-  await shutdown()
-})
