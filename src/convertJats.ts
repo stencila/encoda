1a4462ae8b5cc5d052eb3c560699ee4b49b518cd
Nathan Lisgo <nathan@lisgo.co.uk>
2024-10-17 08:19:53 +0100

    Introduce a simple script to convert from JATS to json

---
+++ src/convertJats.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+import { convert } from './index'
+
+// Convert JATS from stdin and return JSON to console.log
+convert('-', undefined, {
+  from: 'jats',
+  to: 'json',
+  encodeOptions: {
+    isBundle: false,
+  },
+  decodeOptions: {
+    shouldReshape: false,
+  },
+})
+  .then((conversion) => conversion ?? '{}')
+  .then((conversion) => conversion.replaceAll(process.cwd(), ''))
+  .then((conversion) => console.log(conversion))
+  .catch((error) => {
+    console.error('Error during conversion:', error)
+    process.exit(1)
+  })
